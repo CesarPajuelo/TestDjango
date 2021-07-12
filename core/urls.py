13bfc2ce85e490f9e7063b4f650a09890238482a
CesarPajuelo <ce.pajuelo@duocuc.cl>
2021-07-12 14:37:07 -0400

    cambios en la pag parte 2

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('',views.index,name="index"),
     path('main/',views.main ,name="main"),
     path('mecanicos/',views.mecanicos,name="mecanicos"),
+    path('trabajos/',views.trabajos,name="trabajos"),
+     path('quienessomos/',views.quienessomos ,name="quienessomos"),
  
 
     ]
--- core/views.py
+++ core/views.py
@@ -15,6 +15,16 @@ def mecanicos(request):
     context= {} 
     return render(request, 'core/mecanicos.html', context)
 
+def trabajos(request):
+    context= {} 
+    return render(request, 'core/trabajos.html', context)
+
+def quienessomos(request):
+    context= {} 
+    return render(request, 'core/quienessomos.html', context)
+
+
+
 
 
 
